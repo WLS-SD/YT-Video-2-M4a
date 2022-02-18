0f3072ac29468aacf5049cc2b5b23fa629513600
Tech Hacked <77196153+WLS-SD@users.noreply.github.com>
2022-02-18 23:11:56 +0545

    Add files via upload

---
+++ ui/start.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo -e '\e[96m
+                         __________
+                      .~#########%%;~.
+                     /############%%;`\
+                    /######/~\/~\%%;,;,\
+                   |#######\    /;;;;.,.|
+                   |#########\/%;;;;;.,.|
+          XX       |##/~~\####%;;;/~~\;,|       XX
+        XX..X      |#|  o  \##%;/  o  |.|      X..XX
+      XX.....X     |##\____/##%;\____/.,|     X.....XX
+ XXXXX.....XX      \#########/\;;;;;;,, /      XX.....XXXXX
+X |......XX%,.@      \######/%;\;;;;, /      @#%,XX......| X
+X |.....X  @#%,.@     |######%%;;;;,.|     @#%,.@  X.....| X'
+echo -e '\e[92mX  \...X     @#%,.@   ----------------    @ @ >     X  \...X
+ X# \.X        @#%,.@    YT->M4a     @#%,.@.          X# \.X
+                @#%,.@              @#%,.@.
+                  @#%,.@          @#%,.@
+                     @#%,.@      @#%,.@
+                       @#%.,@  @#%,.@
+                         YT - M4a 
+
+
+
+
+
+
+'
+
+				
+
+GREEN="\e[92m"
+
+
+printf "${GREEN}"
+
+sleep 0.2
+
+figlet -f standard "TOOL_by"
+
+sleep 0.2
+
+figlet -f standard "WLS"
+
+sleep 0.2
+
+echo "This Tool is Developed by https://github.com/WLS-SD under GNU GENERAL PUBLIC LICENSE Version 3."
+
---
+++ ui/stop.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+YELLOW="\e[93m"
+
+
+printf "${YELLOW}"
+
+
+
+echo "         ──▐─▌──▐─▌── "
+echo "         ─▐▌─▐▌▐▌─▐▌─ "
+echo "         ─█▄▀▄██▄▀▄█─ "
+echo "         ──▄──██▌─▄── "
+echo "         ▄▀─█▀██▀█─▀▄ "
+echo "         ▐▌▐▌─▐▌─▐▌▐▌ "
+echo "         ─▐─█────█─▌─ "
+echo "         ────▌──▐──── "
+echo "                      "
+
+Green="\e[92m"
+
+
+printf "${Green}"
+
+
+
+echo -e  "   ▂▃▄▅▆▇▓▒░Coded_By_WLS░▒▓▇▆▅▄▃▂"
+echo "      -------------------------"
+
+
+
+LightCyan="\e[96m"
+
+
+printf "${LightCyan}"
+
+
+
+
+
+echo "This Tool is Made by 'https://github.com/WLS-SD' Under GNU GENERAL PUBLIC LICENSE Version 3."
+ 
+
+
+sleep 0.2
+
+
+
