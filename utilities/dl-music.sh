07e06d1fe4c1fe9a98645115ae6367dc123b904a
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-04-19 14:36:36 +0000

    Add files via upload

---
+++ utilities/dl-music.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+cd YT-Video-2-M4a
+cd core
+./main.sh
+
+
+
---
+++ utilities/setup-termux.sh
@@ -0,0 +1,25 @@
+#./bin/bash
+
+termux-setup-storage
+
+apt install figlet -y
+apt install python -y
+apt install ffmpeg -y
+
+pip install youtube_dl
+
+
+./Aliases.sh
+
+cp dl-music.sh $HOME
+
+cd
+cd /sdcard
+mkdir Musics-From-Termux
+
+cd
+chmod 777 dl-music.sh
+
+
+
+
