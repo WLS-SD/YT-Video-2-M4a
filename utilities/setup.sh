c1554916af26ba36ae6ea28e1b3d3dbccdd503a2
WLS-SD <77196153+WLS-SD@users.noreply.github.com>
2022-04-19 14:05:47 +0000

    Add files via upload

---
+++ utilities/setup.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+
+Light_Magenta="\e[95m"
+NONE='\033[00m'
+RED='\033[01;31m'
+GREEN='\033[01;32m'
+YELLOW='\033[01;33m'
+PURPLE='\033[01;35m'
+CYAN='\033[01;36m'
+WHITE='\033[01;37m'
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
+
+
+
+
+echo -e "${BOLD} ${GREEN} Please Choose Your System :~~-"
+echo -e "${PURPLE} 1. Termux (Android)"
+echo -e "${PURPLE} 2. Linux OS (Works on ALL Distros,Especially on Debian Based)"
+
+
+
+printf "${GREEN}"
+
+echo -n "Your System :~~"
+read selected_os
+
+if [ $selected_os = 1 ]
+then
+
+printf "${CYAN}"
+echo "You Have Selected Termux(Android)"
+cd For-Termux
+./setup-termux.sh
+
+elif [ $selected_os = 2 ]
+then
+
+printf "${CYAN}"
+
+echo " You Have Selected Linux Os"
+cd For-Linux
+./setup-linux-system.sh
+
+
+
+
+else
+
+printf "${CYAN}"
+
+echo 'Invalid Option !!!'
+echo 'Please Choose 1 For Termux(Android) & 2 For Linux Os.'
+
+fi
